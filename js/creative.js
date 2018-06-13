38427781b376be4c04be263871613cf894970ba6
Michael Reyes <michael@iconph.net>
2018-06-14 01:10:13 +0800

    Working on projects

--- js/creative.js
+++ js/creative.js
@@ -1,3 +1,28 @@
+jQuery(document).ready(function($) {
+
+    $('#myCarousel').carousel({
+        interval: 5000
+    });
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+        var id = $('.item.active').data('slide-number');
+        $('#carousel-text').html($('#slide-content-'+id).html());
+    });
+});
+
+
 (function($) {
   "use strict"; // Start of use strict
 
@@ -69,3 +94,4 @@
   });
 
 })(jQuery); // End of use strict
+
